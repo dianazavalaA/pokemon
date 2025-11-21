aa482ad06e731520a65e99ede57e33be01c96a50
Diana Zavala <diana_zavalaa@hotmail.com>
2025-11-21 00:16:45 -0600

    Add aside

---
+++ components/Sidebar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Sidebar = () => {
+
+return (
+ <>
+     <aside className="w-20 h-screen bg-[#5F4B8B] text-white flex flex-col items-center py-6 gap-10">
+        
+     </aside>
+ </>
+)
+
+}
