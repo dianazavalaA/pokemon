6d46cb0a81ad904ef595993923ffaae59ff185ac
Diana Zavala <diana@Mac-mini-de-Diana.local>
2025-11-22 00:23:12 -0600

    Add aside, background and icon

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export const Sidebar = () => {
 
 return (
  <>
      <aside className="w-20 h-screen bg-[#5F4B8B] text-white flex flex-col items-center py-6 gap-10">
-        
+        <Link href="/home">
+            <Image
+                src="/ico.png"
+                alt="Icono"
+                width={40}
+                height={40}
+            />
+        </Link>
      </aside>
  </>
 )
